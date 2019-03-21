aae8a11d5a506e6d3c28ddd32541ca2fc7b36e64
Pablo Antonio <pabloa@gmail.com>
2019-03-21 01:13:40 -0300

    Add urlify2 (linear time)

--- ctci/6th-1-3-urlify.py
+++ ctci/6th-1-3-urlify.py
@@ -1,20 +1,44 @@
 import unittest
 
+
 # O(n**2) time, O(n) space
-def urlify(s):
-    l = list(s)
+def urlify(l):
     for i in range(len(l)):
         if l[i] == ' ':
             shift(l, i, 2)
             l[i] = '%'
             l[i + 1] = '2'
             l[i + 2] = '0'
-    return ''.join(l)
 
 
 def shift(l, start, offset):
     for i in range(len(l) - 1, start + offset - 1, -1):
         l[i] = l[i - offset]
+
+
+# O(n) time, O(n) space
+def urlify2(l):
+    def get_true_length(l):
+        for i in range(len(l) - 1, -1, -1):
+            if l[i] != ' ':
+                return i + 1
+
+    def get_true_spaces(l, true_length):
+        return sum(1 if l[i] == ' ' else 0 for i in range(true_length))
+
+    true_length = get_true_length(l)
+    spaces = get_true_spaces(l, true_length)
+    j = true_length - 1 + spaces * 2
+
+    for i in range(true_length - 1, -1, -1):
+        if l[i] != ' ':
+            l[j] = l[i]
+            j -= 1
+        else:
+            l[j] = '0'
+            l[j - 1] = '2'
+            l[j - 2] = '%'
+            j -= 3
 
 
 class Test(unittest.TestCase):
@@ -29,4 +53,9 @@ class Test(unittest.TestCase):
         self.assertEqual(list("holaabcabc"), l)
 
     def test_urlify(self):
-        self.assertEqual("Mr%20John%20Smith", urlify("Mr John Smith    "))
+        l = list("Mr John Smith    ")
+        urlify(l)
+        self.assertEqual(list("Mr%20John%20Smith"), l)
+        l = list("Mr John Smith    ")
+        urlify2(l)
+        self.assertEqual(list("Mr%20John%20Smith"), l)
